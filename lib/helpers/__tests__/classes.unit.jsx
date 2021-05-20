99f45043f732caeb53286002b33d76687211cb3a
杨云萍 <yuy20170301@126.com>
2021-05-20 15:13:35 +0800

    feat(帮助文件夹): 新建一个类名函数
    对类名函数测试

---
+++ lib/helpers/__tests__/classes.unit.jsx
@@ -0,0 +1,19 @@
+import classes from '../classes'
+
+describe('classes', () => {
+    it('接受一个参数', () => {
+      expect(classes('a')).toEqual('a')
+    })
+    it('接受两个参数', () => {
+      expect(classes('a','12')).toEqual('a 12')
+    })
+    it('接受undefined', () => {
+      expect(classes('a',undefined)).toEqual('a')
+    })
+    it('接受多个参数', () => {
+      expect(classes('a',undefined,null,'你好')).toEqual('a 你好')
+    })
+    it('接受0参数', () => {
+      expect(classes()).toEqual('')
+    })
+}) 
---
+++ lib/helpers/classes.ts
@@ -0,0 +1,4 @@
+const classes = (...names: (string | undefined)[]) => {
+  return names.filter(Boolean).join(' ');
+}
+export default classes;
