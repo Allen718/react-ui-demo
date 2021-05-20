86d6adf31a9884c500ce571e5d0750d253ff74c3
杨云萍 <yuy20170301@126.com>
2021-05-20 15:20:36 +0800

    feat(webpack): 配置 svg 和scss

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.svg?$/,
+        loader: 'svg-sprite-loader'
+      },
+      {
+        test: /\.s[ac]ss?$/,
+       use: ['style-loader', 'css-loader', 'sass-loader']
+
       }
     ]
 
