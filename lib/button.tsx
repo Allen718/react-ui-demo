51c353d936687b107a19164849229862975f8599
杨云萍 <yuy20170301@126.com>
2021-05-20 15:18:00 +0800

    feat(button): 关于测试例子

--- lib/button.tsx
+++ lib/button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 const Button = () => {
-  return (<div>
+  return (<div >
     按钮
   </div>)
 };
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,20 @@
 
 
-import Button from './button';
-const a = 1;
-
-export default { Button, a }
+import React from 'react';
+import ReactDom from 'react-dom';
+import Icon from './icon/icon';
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e.target)
+}
+ReactDom.render(<div>
+  <Icon name='wechat'
+    className="wechat"
+    onClick={fn}
+    onMouseEnter={() => {
+      console.log('enter')
+    }}
+    onMouseLeave={() => {
+      console.log('leave')
+    }}
+  />
+</div>, document.querySelector('#root'));
