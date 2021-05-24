819bb5b9f8c9a7e8e31688852e2f2a77b55b269d
杨云萍 <yuy20170301@126.com>
2021-05-24 12:06:58 +0800

    fix(circle ci): 构建报错

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,20 +1,21 @@
 
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon';
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target)
-}
-ReactDom.render(<div>
-  <Icon name='wechat'
-    className="wechat"
-    onClick={fn}
-    onMouseEnter={() => {
-      console.log('enter')
-    }}
-    onMouseLeave={() => {
-      console.log('leave')
-    }}
-  />
-</div>, document.querySelector('#root'));
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import Icon from './icon/icon';
+// const fn: React.MouseEventHandler = (e) => {
+//   console.log(e.target)
+// }
+// ReactDom.render(<div>
+//   <Icon name='wechat'
+//     className="wechat"
+//     onClick={fn}
+//     onMouseEnter={() => {
+//       console.log('enter')
+//     }}
+//     onMouseLeave={() => {
+//       console.log('leave')
+//     }}
+//   />
+// </div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
