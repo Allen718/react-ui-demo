1b26b5929ba3276e5757925a70f30140f578bf17
杨云萍 <yuy20170301@126.com>
2021-05-31 11:46:47 +0800

    feat(helper 文件): 新增一个生成类名的函数

--- lib/helpers/classes.ts
+++ lib/helpers/classes.ts
@@ -1,4 +1,11 @@
 const classes = (...names: (string | undefined)[]) => {
   return names.filter(Boolean).join(' ');
 }
-export default classes;
+
+const scopedClassMarker=(prefix:string)=>{
+ return  function(name?:string){
+    return [prefix,name].filter(Boolean).join('-');
+  }
+};
+
+export  {classes,scopedClassMarker};
--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './importIcons';
 import './icon.scss';
-import classes from '../helpers/classes';
+import {classes} from '../helpers/classes';
+import './icons/wechat.svg'
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string,
-  onClick: React.MouseEventHandler<SVGElement>,
+   onClick?: React.MouseEventHandler<SVGElement>,
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   const { className, name, children, ...restProps } = props
   return (
       <svg className={classes('icon', className)} aria-hidden="true"  {...restProps}>
-        <use xlinkHref={name}></use>
+        <use xlinkHref={`#${name}`}></use>
       </svg>
   )
 };
