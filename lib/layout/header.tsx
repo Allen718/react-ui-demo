0f8bf3268e7c041d90953cad31676cce49e9de66
杨云萍 <yuy20170301@126.com>
2021-06-07 10:57:22 +0800

    fix(gh-pages):官网部署 删除不必要文件

--- lib/layout/header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {scopedClassMarker} from "../helpers/classes";
-interface Props extends React.HTMLAttributes<HTMLElement>{}
-const Header:React.FunctionComponent<Props>=(props)=>{
-  const {className,...rest}=props
-  const sc=scopedClassMarker('fui-layout')
-  return(<div className={sc('header',{extra:className})} {...rest}>{props.children}</div>)
-};
-export default Header;
