ee58f3e00b23d29100e4628c01215c011eb81f67
杨云萍 <yuy20170301@126.com>
2021-06-07 11:11:33 +0800

    删除不必要文件

--- jest.config.ci.js
+++
@@ -1,8 +0,0 @@
-const base = require('./jest.config')
-module.exports = Object.assign({}, base, {
-  reporters: ["default","jest-junit"],
-  collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-})
