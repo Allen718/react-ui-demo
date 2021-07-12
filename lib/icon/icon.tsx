0f847e7c5320b9bae40c2fa149754510ea8bf8fe
杨云萍 <yuy20170301@126.com>
2021-07-12 15:16:22 +0800

    feat(icon 库): 增加几个icon 图标

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -5,16 +5,25 @@ import {classes} from '../helpers/classes';
 import './icons/wechat.svg'
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
-  name: string,
-   onClick?: React.MouseEventHandler<SVGElement>,
+  name: string
+  onClick?: React.MouseEventHandler<SVGElement>
+  size?: number
+  color?: string
+  style?: React.CSSProperties
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const { className, name, children, ...restProps } = props
+  const {className, name, size,style, children, ...restProps} = props
   return (
-      <svg className={classes('icon', className)} aria-hidden="true"  {...restProps}>
-        <use xlinkHref={`#${name}`}></use>
-      </svg>
+    <svg
+      className={classes('icon', className)}
+      aria-hidden="true"
+      style={Object.assign({}, style, {
+      width: size + 'px',
+      height: size + 'px'
+    })} {...restProps}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
   )
 };
 export default Icon;
