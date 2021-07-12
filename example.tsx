e103c53caa233e4cd4333a735844dbb31134d77d
杨云萍 <yuy20170301@126.com>
2021-07-12 15:18:47 +0800

    feat(主页路由添加): 关于新增页面

--- example.tsx
+++ example.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router, Route,NavLink} from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
-import ButtonExample from './lib/button.example';
+import ButtonExample from './lib/button/button.example';
 import DialogExample from './lib/dialog/dialog.example';
 import LayoutExample from './lib/layout/layout.example';
-import logo from './lib/assets/logo.png' ;
+import FormExample from './lib/form/form.example';
+import InputExample from './lib/input/input.example';
+import ScrollExample from './lib/scroll/scroll.example'
+import logo from './lib/assets/logo.png';
 import text from './lib/assets/text.png'
 import './example.scss';
-import {Header, Footer, Content, Aside, Layout} from './lib/layout/layout';
+import { Header, Footer, Content, Aside, Layout } from './lib/layout/layout';
 
 ReactDOM.render(
   <Router>
     <Layout className={'site-page'}>
       <Header className={'site-header'}>
         <div className="logo">
-          <img src={logo}/>
+          <img src={logo} />
         </div>
         <div className={'text'}>
-          <img src={text}/>
+          <img src={text} />
         </div>
       </Header>
       <Layout>
@@ -32,19 +35,31 @@ ReactDOM.render(
               <NavLink to="/button">Button</NavLink>
             </li>
             <li>
+              <NavLink to="/input">Input</NavLink>
+            </li>
+            <li>
               <NavLink to="/dialog">dialog</NavLink>
             </li>
             <li>
               <NavLink to="/layout">layout</NavLink>
             </li>
+            <li>
+              <NavLink to="/form">form</NavLink>
+            </li>
+            <li>
+              <NavLink to="/scroll">scroll</NavLink>
+            </li>
           </ul>
-        
+
         </Aside>
         <Content className={'site-main'}>
-          <Route path="/icon" component={IconExample}/>
-          <Route path="/button" component={ButtonExample}/>
-          <Route path="/dialog" component={DialogExample}/>
-          <Route path="/layout" component={LayoutExample}/>
+          <Route path="/icon" component={IconExample} />
+          <Route path="/button" component={ButtonExample} />
+          <Route path="/input" component={InputExample} />
+          <Route path="/dialog" component={DialogExample} />
+          <Route path="/layout" component={LayoutExample} />
+          <Route path="/form" component={FormExample} />
+          <Route path="/scroll" component={ScrollExample} />
         </Content>
       </Layout>
       <Footer className="site-footer">
